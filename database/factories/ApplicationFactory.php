bdaf689803c28f6f9acf294aafc5b266c734e1e2
Uluk <ulukulanbekov19@gmail.com>
2024-06-07 07:00:11 +0600

    init project

---
+++ database/factories/ApplicationFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Application;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Application>
+ */
+class ApplicationExceptionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Application::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'job_id' => function () {
+                return \App\Models\Position::factory()->create()->id;
+            },
+            'user_id' => function () {
+                return \App\Models\User::factory()->create()->id;
+            },
+            'cover_letter' => $this->faker->paragraph,
+            // Add other attributes as needed
+        ];
+    }
+}
